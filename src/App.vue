165de9a359ca4531879d37efd5c99a6d7033c621
Juan Carlos <jcmendoza1986@gmail.com>
2025-08-29 19:43:28 -0400

    cambio stilos

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div>
     <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="./assets/Emblem_bolivar.png" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/tigre.jpg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <router-view />
